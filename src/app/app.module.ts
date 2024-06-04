0772a1f867d3b932ca04810ce099a30c47e35fe4
kennethihezie <kennethihezie06@gmail.com>
2024-06-04 16:26:38 +0100

    added mongodb connection  string from docker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from 'src/users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ UsersModule ],
+  imports: [ UsersModule, MongooseModule.forRoot('mongodb://admin:password@localhost:27017', { dbName: 'user_db' }) ],
   controllers: [AppController],
   providers: [AppService],
 })
